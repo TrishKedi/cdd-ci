7d4d2e9654ce41fed1fc48359e05219d637b7ba4
TrishKedi <patimkedi@gmail.com>
2025-12-31 11:45:44 +0100

    check no changed files

--- src/app.py
+++ src/app.py
@@ -13,37 +13,39 @@ def run_ci_pipeline():
         # print(f"\n{chunk}\n")
 
     query_chunks = code_analyzer.extract_code_chuncks()
-    query = code_analyzer.get_processed_code(query_chunks)
-    print("\n=======QUERY CHUNKS========\n")
-    # print(f"\n{query}\n")
-    print(f"\n{len(query)}\n")
 
-    candidate_chunks = code_analyzer.extract_code_chuncks(query_files=False)
-    candidate = code_analyzer.get_processed_code(candidate_chunks)
-    print("\n=======CANDIDATE CHUNKS========\n")
-    # print(f"\n{candidate}\n")
-    print(f"\n{len(candidate)}\n")
 
-    
-    query_embeddings = embedder.generate_embeddings(query)
-    candidate_embeddings = embedder.generate_embeddings(candidate)
-    similarity_lookup.build_index(candidate_embeddings)
+    if query_chunks:
+        query = code_analyzer.get_processed_code(query_chunks)
+        print("\n=======QUERY CHUNKS========\n")
+        # print(f"\n{query}\n")
+        print(f"\n{len(query)}\n")
+        candidate_chunks = code_analyzer.extract_code_chuncks(query_files=False)
+        candidate = code_analyzer.get_processed_code(candidate_chunks)
+        print("\n=======CANDIDATE CHUNKS========\n")
+        # print(f"\n{candidate}\n")
+        print(f"\n{len(candidate)}\n")
 
-    search_results = similarity_lookup.run_semantic_search(query_embeddings)
+        
+        query_embeddings = embedder.generate_embeddings(query)
+        candidate_embeddings = embedder.generate_embeddings(candidate)
+        similarity_lookup.build_index(candidate_embeddings)
 
-    all_query_chunks = code_analyzer.get_processed_code(query_chunks, all=True)
-    print(len(all_query_chunks))
-    all_candidate_chunks = code_analyzer.get_processed_code(candidate_chunks, all=True)
+        search_results = similarity_lookup.run_semantic_search(query_embeddings)
 
-    clones = similarity_lookup.process_results(
-        search_results, 
-        all_query_chunks, 
-        all_candidate_chunks
-    )
+        all_query_chunks = code_analyzer.get_processed_code(query_chunks, all=True)
+        print(len(all_query_chunks))
+        all_candidate_chunks = code_analyzer.get_processed_code(candidate_chunks, all=True)
 
-    print("\n=======CLONES========\n")
-    print(f"\n{clones}\n")
-    print(f"\n{len(clones)}\n")
+        clones = similarity_lookup.process_results(
+            search_results, 
+            all_query_chunks, 
+            all_candidate_chunks
+        )
+
+        print("\n=======CLONES========\n")
+        print(f"\n{clones}\n")
+        print(f"\n{len(clones)}\n")
 
 
      
